6a67646d4acf96b4b476d053dc24a974783aac1f
KevinVillela <kevin@villela.com>
2020-03-22 12:43:13 -0700

    feat(verily): First working version of selecting a ZIP file and merging it.

--- printer.py
+++ printer.py
@@ -1,21 +1,73 @@
+import glob
+import os
+import subprocess
 import tkinter as tk
+from tkinter import filedialog, LEFT
+from PyPDF2 import PdfFileMerger
+import webbrowser
+from zipfile import ZipFile
 
 root = tk.Tk()
+root.title("Verily Patient Data Printer")
 
-canvas1 = tk.Canvas(root, width=300, height=300)
-canvas1.pack()
+WIDTH = 300
+canvas1 = tk.Canvas(root, width=WIDTH, height=300)
+title = tk.Label(root, text="Hello, and welcome to the Verily Patient Data Printer!", fg="black", wraplength=WIDTH, font=("Helvetica", 16), padx=20, pady=20)
+title.grid(row=0)
 
-
-def hello():
-    label1 = tk.Label(root, text='Hello World!', fg='green', font=('helvetica', 12, 'bold'))
-    canvas1.create_window(150, 200, window=label1)
-
-
-button1 = tk.Button(text='Click Me', command=hello, bg='brown', fg='white')
-canvas1.create_window(150, 150, window=button1)
-
+status_label = tk.StringVar()
+status_label.set('')
+def open(file: str):
+    webbrowser.open_new(file)
 
 
+def extract_in_file(folder: str, result_file: str):
+    files = glob.iglob(folder + '**/*.pdf', recursive=True)
+    pdf_files = [f for f in files if (os.path.isfile(f) and (f).endswith('pdf'))]
+    if len(pdf_files) == 0:
+        status_label.set("No PDF files found in that directory.")
+        return
+    merger = PdfFileMerger()
+    for pdf in pdf_files:
+        merger.append(pdf)
+    merger.write(result_file)
+    merger.close()
+    status_label.set("Merged " + str(len(pdf_files)) + " PDFs into\n" + result_file)
+
+    step_3_label = tk.Label(root, text="Step 3: Open and print the merged PDF: ", fg="black", wraplength=250, justify=LEFT)
+    step_3_label.grid(column=0, row=4)
+    open_button = tk.Button(text='Open PDF', command=lambda: open(result_file), bg='brown', fg='white')
+    open_button.grid(column=1, row=4)
+
+def merge():
+    file_selected = filedialog.askopenfilename(filetypes=[("ZIP Files", ".zip")])
+    if not file_selected:
+        return
+    folder_selected = os.path.dirname(file_selected)
+    with ZipFile(file_selected, 'r') as zipObj:
+        # Extract all the contents of zip file in different directory
+        zipObj.extractall('pdfs')
+
+    result_file = folder_selected + "/merged.pdf"
+    if os.path.exists(result_file):
+        os.remove(result_file)
+
+    extract_in_file(folder_selected + "/pdfs", result_file)
+
+status = tk.Label(root, textvariable=status_label, fg="green", wraplength=250)
+status.grid(column=0, row=3)
+
+step_1_label = tk.Label(root, text="Step 1: Download the ZIP file", fg="black", wraplength=250, justify=LEFT)
+step_1_label.grid(column=0, row=1)
+
+merge_button_label = tk.Label(root, text="Step 2: Select the ZIP file:", fg="black", wraplength=250, justify=LEFT)
+merge_button_label.grid(column=0, row=2)
+merge_button = tk.Button(text='Choose ZIP', command=merge, bg='brown', fg='white')
+merge_button.grid(column=1, row=2)
+
+step_4_label = tk.Label(root, text="Step 4: Download the CSV File", fg="black", wraplength=250, justify=LEFT)
+step_4_label.grid(column=0, row=6)
+step_5_label = tk.Label(root, text="Step 5: Install the Brady Printer software", fg="black", wraplength=250, justify=LEFT)
+step_5_label.grid(column=0, row=7)
 if __name__ == '__main__':
     root.mainloop()
-    print('hi')
